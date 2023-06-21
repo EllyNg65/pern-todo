f9d7acc082e92e65b220e595eba8a792d5a0f51d
EllyNg65 <elizabethngxinyi@gmail.com>
2023-06-21 09:43:33 +0800

    add postgresql queries to frontend

---
+++ README.md
@@ -0,0 +1,5 @@
+### Reference
+
+PERN Stack Course - Postgres, Express, React, and Node \
+freeCodeCamp.org \
+https://www.youtube.com/watch?v=ldYcgPKEZC8
---
+++ client/src/components/EditTodo.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+
+const EditTodo = ({ todo }) => {
+  const [description, setDescription] = useState(todo.description);
+
+  const updateDescription = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      await fetch(`http://localhost:5000/todos/${todo.todo_id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      window.location = "/";
+    } catch (err) {
+      console.log("Error: ", err.message);
+    }
+  };
+
+  const resetFormField = () => {
+    setDescription(todo.description);
+  };
+
+  useEffect(() => {
+    document.addEventListener("hidden.bs.modal", resetFormField);
+
+    return () => {
+      document.removeEventListener("hidden.bs.modal", resetFormField);
+    };
+  }, []);
+
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-warning"
+        data-bs-toggle="modal"
+        data-bs-target={`#id${todo.todo_id}`}
+      >
+        Edit
+      </button>
+
+      <div
+        className="modal fade"
+        id={`id${todo.todo_id}`}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Edit Todo
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+                onClick={resetFormField}
+              ></button>
+            </div>
+            <div className="modal-body">
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+                onClick={resetFormField}
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={(e) => updateDescription(e)}
+              >
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditTodo;
